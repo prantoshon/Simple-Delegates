c127d5be24ca26fcd7cebe3757f2fa37add7e998
Pranto <prantoshon14@gmail.com>
2020-08-09 13:25:18 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public delegate float Add(int x, int y);
+
+
+namespace Delegates
+{
+    class Program
+    {
+        private static float sum(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Add obj = new Add(sum);
+            Console.WriteLine(obj.Invoke(4,7));
+            Console.Read();
+        }
+    }
+}
